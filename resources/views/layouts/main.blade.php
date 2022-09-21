671a05a85d96d5af240446bcbc99688ffdaf66c6
AntonioAlves <92405690+AntonioAlvesFilho@users.noreply.github.com>
2022-09-21 09:29:42 -0300

    Update main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -42,7 +42,7 @@
 																<div class="collapse navbar-collapse" id="navbarNavDropdown">
 																				<ul class="navbar-nav">
 																								<li class="nav-item">
-																												<a class="nav-link active" aria-current="page" href="#">Home</a>
+																												<a class="nav-link active" aria-current="page" href="/">Home</a>
 																								</li>
 																								@guest
 																												<li class="nav-item">
