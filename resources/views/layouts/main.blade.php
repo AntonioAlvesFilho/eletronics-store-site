65d5337f3024e07f9a8b88a090998b59a5e35d04
Ubuntu <ubuntu@ip-172-31-22-131.ec2.internal>
2022-12-06 15:39:53 +0000

    remove-jqueryfile-to-jqueryClound

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,7 +8,6 @@
 				<meta name="csrf-token" content="{{ csrf_token() }}">
 				<link rel="stylesheet" href="/css/styles.css">
 				<link rel="stylesheet" href=".">
-				<script src="/js/scripts.js"></script>
 				<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
 								integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 				<link rel="preconnect" href="https://fonts.googleapis.com">
@@ -18,6 +17,11 @@
 				<link
 								ref="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
 								rel="stylesheet">
+				<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+								integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous">
+				</script>
+				<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+				<script src="/js/script.js"></script>
 
 				<title> @yield('title') </title>
 </head>
@@ -130,11 +134,7 @@
 												<li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
 								</ul>
 				</footer>
-				<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
-								integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous">
-				</script>
-				<script src="/js/jquery-3.6.1.js"></script>
-				<script src="/js/script.js"></script>
+
 </body>
 
 </html>
